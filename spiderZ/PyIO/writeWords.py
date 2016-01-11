5f98531180cca6783155ee280d0e129307ddceb6
gandaruvu <gandaruvu118@gmail.com>
2016-01-11 15:52:32 +0800

    分词

--- spiderZ/PyIO/writeWords.py
+++ spiderZ/PyIO/writeWords.py
@@ -2,17 +2,21 @@
 # coding=utf-8
 import re
 import pymongo
+from WordSplit.splitProxy import SplitProxy
 
 client = pymongo.MongoClient('localhost', 27017)
 db = client.spiderDB
 collection = db.spider
 collection.remove()
 
+proxy = SplitProxy()
 class Write():
     @staticmethod
     def write(url, word_list):
         try:
             for words in word_list:
-                collection.insert({"content": words, "url": url})
+                ws = proxy.split(words)
+                for w in ws:
+                    collection.insert({"content": w, "url": url})
         except Exception, e:
             print url, word_list, e
